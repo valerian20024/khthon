8f092288ac74fc9bd89d42a3084e72047d0a2daf
valerian wislez <Valerian.Wislez@student.uliege.be>
2026-03-04 16:43:02 +0100

    Added base Node class.

--- ast.hpp
+++ ast.hpp
@@ -7,5 +7,26 @@
 #include <optional>     // for std::optional
 #include "parser.hpp"   // for Bison location
 
+// Forward declarations for Visitor. Avoid circular dependencies. 
+class ProgramNode;
+class ClassNode;
+
+template <typename R> class Visitor {
+public:
+    // Pure virtual methods and virtual destructor (rule of zero)
+    virtual R visit(const ProgramNode& node) = 0;
+    virtual R visit(const ClassNode& node) = 0;
+    virtual ~Visitor() = default;
+};
+
+class Node {
+private:
+    Khthon::location loc;
+public:
+    virtual void accept(Visitor<std::string>& v) const = 0;
+    virtual ~Node() = default;
+};
+
+
 
 #endif  // AST_HPP
