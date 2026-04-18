ad15bda79cabd6b8e17fd4b6206e4029a4f485b5
valerian wislez <Valerian.Wislez@student.uliege.be>
2026-04-18 17:10:52 +0200

    Added a small shell script to test the docker instance.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Starts the Docker container and navigate to the project working directory.
+
+docker run --rm -it \
+  -v "$(pwd):/home/vagrant/compilers" \
+  -w "/home/vagrant/compilers" \
+  cffs/compilers /bin/sh
