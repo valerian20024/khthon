8feeb5d4eb43d694b91b3b4c69e6bbd1195e4204
valerian wislez <Valerian.Wislez@student.uliege.be>
2026-04-19 12:15:00 +0200

    Moved around the classvisitor methods in cpp file to match the hpp file

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -11,123 +11,6 @@ using namespace std;
 using Khthon::Type;
 
 namespace Khthon {
-    //todo put it at the end of the file
-    void ClassesVisitor::visit(const ProgramNode& node) const {
-        
-        // Inject the built-in Object class
-        Khthon::location builtin_loc;  // default location, no source file
-        ClassInfo object_info("Object", "Object", builtin_loc);
-
-        // Object's built-in methods
-        object_info.add_method(MethodInfo(
-            "print",
-            Type("Object"),
-            { FormalInfo("s", Type::String(), builtin_loc) },
-            builtin_loc
-        ));
-
-        object_info.add_method(MethodInfo(
-            "printBool",
-            Type("Object"),
-            { FormalInfo("b", Type::Bool(), builtin_loc) },
-            builtin_loc
-        ));
-
-        object_info.add_method(MethodInfo(
-            "printInt32",
-            Type("Object"),
-            { FormalInfo("i", Type::Int32(), builtin_loc) },
-            builtin_loc
-        ));
-
-        object_info.add_method(MethodInfo(
-            "inputLine",
-            Type::String(),
-            {},  // no formals
-            builtin_loc
-        ));
-
-        object_info.add_method(MethodInfo(
-            "inputBool",
-            Type::Bool(),
-            {},
-            builtin_loc
-        ));
-
-        object_info.add_method(MethodInfo(
-            "inputInt32",
-            Type::Int32(),
-            {},
-            builtin_loc
-        ));
-
-        checker_.add_class(object_info);
-
-        // Reading concrete classes of the program.
-        for (const auto& c : node.classes())
-            c->accept(*this);
-    }
-
-    void ClassesVisitor::visit(const ClassNode& node) const {
-        const string& class_name = node.name();
-
-        // Not adding duplicate classes.
-        if (checker_.class_exists(class_name)) {
-            driver_.semantic_error(
-                node.location(), 
-                "class '" + class_name + "' is defined more than once"
-            );
-            return;
-        }
-
-        ClassInfo class_info(class_name, node.parent(), node.location());
-
-        // Fields.
-        for (const auto& field : node.fields()) {
-            FieldInfo field_info(
-                field->name(), 
-                field->type(), 
-                field->location()
-            );
-
-            if (!class_info.add_field(std::move(field_info))) {
-                driver_.semantic_error(
-                    field->location(), 
-                    "field '" + field->name() + "' is defined more than once"
-                );
-            }
-        }
-
-        // Methods.
-        for (const auto& method : node.methods()) {
-            // Formals.
-            vector<FormalInfo> formals_infos;
-            for (const auto& formal : method->formals()) {
-                formals_infos.emplace_back(
-                    formal->name(), 
-                    formal->type(), 
-                    formal->location()
-                );
-            }
-            
-            MethodInfo method_info(
-                method->name(), 
-                method->type(), 
-                std::move(formals_infos), 
-                method->location()
-            );
-
-            if (!class_info.add_method(std::move(method_info))) {
-                driver_.semantic_error(
-                    method->location(),
-                    "method '" + method->name() + "' is defined more than once"
-                );
-            }
-        }
-
-        //class_table_.emplace(class_name, std::move(class_info));
-        checker_.add_class(class_info);
-    }
 
 
         /*================================================++
@@ -331,6 +214,7 @@ namespace Khthon {
         ||                 TYPESVISITOR                   ||
         ++================================================*/
 
+        
     bool TypesVisitor::conforms(const Type& given, const Type& expected) const {
         
         // Undefined types should never reach type checking.
@@ -727,4 +611,128 @@ namespace Khthon {
 
         node.set_type(Type::Unit());
     }
+
+
+        /*================================================++
+        ||                 CLASS VISITOR                  ||
+        ++================================================*/
+
+
+    void ClassesVisitor::visit(const ProgramNode& node) const {
+        
+        // Inject the built-in Object class
+        Khthon::location builtin_loc;  // default location, no source file
+        ClassInfo object_info("Object", "Object", builtin_loc);
+
+        // Object's built-in methods
+        object_info.add_method(MethodInfo(
+            "print",
+            Type("Object"),
+            { FormalInfo("s", Type::String(), builtin_loc) },
+            builtin_loc
+        ));
+
+        object_info.add_method(MethodInfo(
+            "printBool",
+            Type("Object"),
+            { FormalInfo("b", Type::Bool(), builtin_loc) },
+            builtin_loc
+        ));
+
+        object_info.add_method(MethodInfo(
+            "printInt32",
+            Type("Object"),
+            { FormalInfo("i", Type::Int32(), builtin_loc) },
+            builtin_loc
+        ));
+
+        object_info.add_method(MethodInfo(
+            "inputLine",
+            Type::String(),
+            {},  // no formals
+            builtin_loc
+        ));
+
+        object_info.add_method(MethodInfo(
+            "inputBool",
+            Type::Bool(),
+            {},
+            builtin_loc
+        ));
+
+        object_info.add_method(MethodInfo(
+            "inputInt32",
+            Type::Int32(),
+            {},
+            builtin_loc
+        ));
+
+        checker_.add_class(object_info);
+
+        // Reading concrete classes of the program.
+        for (const auto& c : node.classes())
+            c->accept(*this);
+    }
+
+    void ClassesVisitor::visit(const ClassNode& node) const {
+        const string& class_name = node.name();
+
+        // Not adding duplicate classes.
+        if (checker_.class_exists(class_name)) {
+            driver_.semantic_error(
+                node.location(), 
+                "class '" + class_name + "' is defined more than once"
+            );
+            return;
+        }
+
+        ClassInfo class_info(class_name, node.parent(), node.location());
+
+        // Fields.
+        for (const auto& field : node.fields()) {
+            FieldInfo field_info(
+                field->name(), 
+                field->type(), 
+                field->location()
+            );
+
+            if (!class_info.add_field(std::move(field_info))) {
+                driver_.semantic_error(
+                    field->location(), 
+                    "field '" + field->name() + "' is defined more than once"
+                );
+            }
+        }
+
+        // Methods.
+        for (const auto& method : node.methods()) {
+            // Formals.
+            vector<FormalInfo> formals_infos;
+            for (const auto& formal : method->formals()) {
+                formals_infos.emplace_back(
+                    formal->name(), 
+                    formal->type(), 
+                    formal->location()
+                );
+            }
+            
+            MethodInfo method_info(
+                method->name(), 
+                method->type(), 
+                std::move(formals_infos), 
+                method->location()
+            );
+
+            if (!class_info.add_method(std::move(method_info))) {
+                driver_.semantic_error(
+                    method->location(),
+                    "method '" + method->name() + "' is defined more than once"
+                );
+            }
+        }
+
+        //class_table_.emplace(class_name, std::move(class_info));
+        checker_.add_class(class_info);
+    }
+
 }
