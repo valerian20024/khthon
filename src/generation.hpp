a321961a945399dc86ac476968d7e793226a1dcf
valerian wislez <Valerian.Wislez@student.uliege.be>
2026-04-30 11:00:31 +0200

    Added files for generation

---
+++ src/generation.hpp
@@ -0,0 +1,16 @@
+#ifndef GENERATION_HPP
+#define GENERATION_HPP
+
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Type.h"
+
+namespace Khthon {
+
+}
+
+#endif
